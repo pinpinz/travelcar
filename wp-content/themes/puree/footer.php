b06be2a1c26ab5e2f94bf463b51efba5a2e71698
pinpinz <kvinnn12@gmail.com>
2024-01-13 00:47:37 +0700

    perubahan wording dan infinity carousel

--- wp-content/themes/puree/footer.php
+++ wp-content/themes/puree/footer.php
@@ -1,154 +1,79 @@
-       
-<script>
+ <footer>
+  <div class="bb ze ki xn 2xl:ud-px-0">
+    <!-- Footer Top -->
+   
+    <!-- Footer Top -->
 
-    function animateFromY(elem, direction) {
-        direction = direction || 1;
-        var x = 0,
-            y = direction * 200;
-            
-        elem.style.transform = "translate(" + x + "px, " + y + "px)";
-        elem.style.opacity = "0";
-        gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-            duration: 3, 
-            x: 0,
-            y: 0, 
-            autoAlpha: 1, 
-            ease: "expo", 
-            overwrite: "auto"
-        });
-    }
-    function animateFromX(elem, direction) {
-        direction = direction || 1;
-        var x = direction * 200,
-            y = 0;
-            
-        elem.style.transform = "translate(" + x + "px, " + y + "px)";
-        elem.style.opacity = "0";
-        gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-            duration: 3, 
-            x: 0,
-            y: 0, 
-            autoAlpha: 1, 
-            ease: "expo", 
-            overwrite: "auto"
-        });
-    }
+    <!-- Footer Bottom -->
+    <div class="bh ch pm tc uf sf yo wf xf ap cg fp bj">
+      <div class="animate_top">
+        <ul class="tc wf gg">
+          <li><a href="#" class="xl">English</a></li>
+          <li><a href="#" class="xl">Privacy Policy</a></li>
+          <li><a href="#" class="xl">Support</a></li>
+        </ul>
+      </div>
 
-    function hide(elem) {
-        gsap.set(elem, {autoAlpha: 0});
-    }
+      <div class="animate_top">
+          <p>&copy; 2024 Copyright. Created by puree</p>
+      </div>
+    </div>
+    <!-- Footer Bottom -->
+  </div>
+</footer>
 
-    document.addEventListener("DOMContentLoaded", function() {
-        gsap.registerPlugin(ScrollTrigger);
-        
-        gsap.utils.toArray(".gs_revealX").forEach(function(elem) {
-            hide(elem); // assure that the element is hidden when scrolled into view
-            
-            ScrollTrigger.create({
-                trigger: elem,
-                markers: false,
-                onEnter: function() { animateFromX(elem) }, 
-                onEnterBack: function() { animateFromX(elem, -1) },
-                onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-            });
-        })
+    <!-- ===== Footer End ===== -->
 
-        gsap.utils.toArray(".gs_revealY").forEach(function(elem) {
-            hide(elem); // assure that the element is hidden when scrolled into view
-            
-            ScrollTrigger.create({
-                trigger: elem,
-                markers: false,
-                onEnter: function() { animateFromY(elem) }, 
-                onEnterBack: function() { animateFromY(elem, -1) },
-                onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-            });
-        })
-    });
+    <!-- ====== Back To Top Start ===== -->
+    <button
+  class="xc wf xf ie ld vg sr gh tr g sa ta _a"
+  @click="window.scrollTo({top: 0, behavior: 'smooth'})"
+  @scroll.window="scrollTop = (window.pageYOffset > 50) ? true : false"
+  :class="{ 'uc' : scrollTop }"
+>
+  <svg class="uh se qd" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+  </svg>
+</button>
 
-</script>
-        <body>
-                        <div class="lg:px-60" style="background-color:#F2F2F2;">
-                            <div class="grid grid-cols-2 gap-8 px-6 py-4 md:grid-cols-4">
-                            <img class="w-40" src="<?php bloginfo('template_directory');?>/images/AssetLogo12.png" alt="logo">
-                            </div>
-                            <div class="grid grid-cols-2 gap-8 px-6 py-2 md:grid-cols-4">
-                                <div>
-                                    <h2 class="mb-2 text-xs font-medium text-black">Head Office - Surabaya, Indonesia</h2>
-                                    <ul class="text-gray-500 dark:text-gray-400">
-                                        <p class="-mt-4">-</p>
-                                        <li class="mb-8">
-                                            <a href="#" class="flex justify-start text-black hover:underline text-xs ">Jl. Kalianget No.15B Perak Utara, <br>Surabaya, Jawa Timur 60615 <br>Ph.+62333287604</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h2 class="mb-2 text-xs font-medium text-black">Operational Office - Medan, Indonesia</h2>
-                                    <ul class="text-gray-500 dark:text-gray-400">
-                                        <p class="-mt-4">-</p>
-                                        <li class="mb-8">
-                                            <a href="#" class="flex justify-start text-black hover:underline text-xs ">Graha Harmoni 5th Floor, <br>Jl Gaharu no.28, Medan Sumatera Utara 20235 <br>Ph.+62614141755</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h2 class="mb-2 text-xs font-medium text-black">Operational Office - Ha Noi, Vietnam</h2>
-                                    <ul class="text-gray-500 dark:text-gray-400">
-                                        <p class="-mt-4">-</p>
-                                        <li class="mb-8">
-                                            <a href="#" class="flex justify-start text-black hover:underline text-xs ">18444 Due Giang Street, <br>Long Beach District, Hanoi, Vietnam <br>Ph.+843957711954</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h2 class="mb-2 text-xs font-medium text-black">Reach Us</h2>
-                                    <div class="container pt-1">
-                                        <div class="mb-6 flex justify-start">
-                                                <a
-                                                href="#!"
-                                                type="button"
-                                                class="m-1 h-6 w-6 mr-4 rounded-full border-black uppercase leading-normal text-black transition duration-20 ease-in-out hover:opacity-50 hover:opacity-50 focus:outline-none focus:ring-0"
-                                                data-te-ripple-init
-                                                data-te-ripple-color="light">
-                                                <img class="w-40" src="<?php bloginfo('template_directory');?>/images/Asset 7.png" alt="wa">
-                                                </a>
+    <!-- ====== Back To Top End ===== -->
 
-                                                <a
-                                                href="#!"
-                                                type="button"
-                                                class="m-1 h-6 w-6 mr-4 border-black uppercase leading-normal text-black transition duration-20 ease-in-out hover:opacity-50 hover:opacity-50 focus:outline-none focus:ring-0"
-                                                data-te-ripple-init
-                                                data-te-ripple-color="light">
-                                                <img class="w-40" src="<?php bloginfo('template_directory');?>/images/Asset 8.png" alt="wa">
-                                                </a>
+    <script>
+      //  Pricing Table
+      const setup = () => {
+        return {
+          isNavOpen: false,
 
-                                                <a
-                                                href="#!"
-                                                type="button"
-                                                class="m-1 h-6 w-6 mr-4 border-black uppercase leading-normal text-black transition duration-20 ease-in-out hover:opacity-50 hover:opacity-50 focus:outline-none focus:ring-0"
-                                                data-te-ripple-init
-                                                data-te-ripple-color="light">
-                                                <img class="w-40" src="<?php bloginfo('template_directory');?>/images/Asset 9.png" alt="wa">
-                                                </a>
+          billPlan: 'monthly',
 
-                                                <a
-                                                href="#!"
-                                                type="button"
-                                                class="m-1 h-6 w-6 mr-4 border-black uppercase leading-normal text-black transition duration-20 ease-in-out hover:opacity-50 hover:opacity-50 focus:outline-none focus:ring-0"
-                                                data-te-ripple-init
-                                                data-te-ripple-color="light">
-                                                <img class="w-40" src="<?php bloginfo('template_directory');?>/images/Asset 10.png" alt="wa">
-                                                </a> 
-                                        </div>
-                                        <p class="mb-4 underline underline-offset-4 text-xs">
-                                        2022 DCM Logistics website design by Brandingg & Project
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-            </div>
-        <?php wp_footer(); ?>
-
-    </body>
+          plans: [
+            {
+              name: 'Starter',
+              price: {
+                monthly: 29,
+                annually: 29 * 12 - 199,
+              },
+              features: ['400 GB Storaget', 'Unlimited Photos & Videos', 'Exclusive Support'],
+            },
+            {
+              name: 'Growth Plan',
+              price: {
+                monthly: 59,
+                annually: 59 * 12 - 100,
+              },
+              features: ['400 GB Storaget', 'Unlimited Photos & Videos', 'Exclusive Support'],
+            },
+            {
+              name: 'Business',
+              price: {
+                monthly: 139,
+                annually: 139 * 12 - 100,
+              },
+              features: ['400 GB Storaget', 'Unlimited Photos & Videos', 'Exclusive Support'],
+            },
+          ],
+        };
+      };
+    </script>
+  <script defer src="<?php bloginfo('template_directory');?>/script/bundle.js"></script></body>
 </html>
--- wp-content/themes/puree/functions.php
+++ wp-content/themes/puree/functions.php
@@ -2,7 +2,7 @@
     if ( ! function_exists( 'bcm_setup' ) ) :
         function bcm_setup() {
             // META BOX - START
-                require_once get_template_directory().'/inc/meta.php';
+                // require_once get_template_directory().'/inc/meta.php'; // tak disable masih ambigu buat apa. file tak delete
             // META BOX - END
             
             // WORDPRESS CORE - START
@@ -26,22 +26,9 @@
 
             // REGISTER SCRIPTS - START
                 function bcm_register_scripts(){
-                    wp_enqueue_script('bcm-all', get_template_directory_uri().'/inc/asset/js/all.js', array(),'', true);
-                    wp_localize_script( 'bcm-all', 'object_page', array( 'template_url' => get_stylesheet_directory_uri() ) ); // get url theme in js
-
-                    wp_enqueue_script('bcm-tailwind','https://cdn.tailwindcss.com', array(), '', false);	
-                     wp_enqueue_script('bcm-ionicons','https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js', array(), '', false);	
-                    // wp_enqueue_script('bcm-bootstrap-js', get_template_directory_uri().'/inc/cdn/bootstrap/js/bootstrap.bundle.min.js', array(), '', true);	
-                    wp_enqueue_script('bcm-ajax', get_template_directory_uri().'/inc/cdn/ajax/jquery.min.js', array(), '', false);
-                    wp_enqueue_script('bcm-jquery', get_template_directory_uri().'/inc/cdn/jquery/jquery-3.6.0.js', array(), '', false);	
-                    wp_enqueue_script('bcm-isotop','https://npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js', array(), '', false);	
-                    wp_enqueue_script('bcm-fontfaceobserver', 'https://cdnjs.cloudflare.com/ajax/libs/fontfaceobserver/2.3.0/fontfaceobserver.js', array(), '', false);	
-                    wp_enqueue_script('bcm-slick', get_template_directory_uri().'/inc/cdn/slick/slick.min.js', array(), '', false);	
-                    wp_enqueue_script('bcm-gsap', get_template_directory_uri().'/inc/cdn/gsap/gsap.min.js', array(),'', true);  
-                    wp_enqueue_script('bcm-gsap-ScrollTrigger', get_template_directory_uri().'/inc/cdn/gsap/ScrollTrigger.min.js', array(),'', true);  
+                    wp_enqueue_script('bcm-bundle', get_template_directory_uri().'/script/bundle.js', array(),'', true);
                     if ( is_page( 'front-page' ) || is_page( 'home' ) ) {
-                        wp_enqueue_script('bcm-pixi', get_template_directory_uri().'/inc/cdn/pixi/pixi.js', array(),'', true);  
-                        wp_enqueue_script('bcm-front-page', get_template_directory_uri().'/inc/asset/js/front-page.js', array(),'', true);  
+                       
                     }   
                 }
                 add_action('wp_enqueue_scripts', 'bcm_register_scripts');
@@ -50,13 +37,7 @@
             // REGISTER STYLES - START
                 function bcm_register_styles(){
                     wp_enqueue_style('bcm-style', get_template_directory_uri().'/style.css', array(), '1', 'all');
-                    wp_enqueue_style('bcm-font-arimo', get_template_directory_uri().'/inc/asset/font/font.css', array(), '', 'all');
-                    wp_enqueue_style('bcm-all', get_template_directory_uri().'/inc/asset/css/all.css', array(), '', 'all');
-                    wp_enqueue_style('bcm-slick', get_template_directory_uri().'/inc/cdn/slick/slick.css', array());
-	                wp_enqueue_style('bcm-slick-theme', get_template_directory_uri().'/inc/cdn/slick/slick-theme.css', array());
-                    // wp_enqueue_style('bcm-bootstrap-css', get_template_directory_uri().'/inc/cdn/bootstrap/css/bootstrap.min.css', array(), '', 'all');
                     if ( is_page( 'front-page' ) || is_page( 'home' ) ) {
-                        wp_enqueue_style('bcm-front-page', get_template_directory_uri().'/inc/asset/css/front-page.css', array(), '', 'all'); 
                     }  
                 }
                 
